9030f9a8e6e4903dda8655e949a787d7bac373df
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-09-01 10:26:22 -0700

    Fix sessions controller redirect paths

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
   def new
     if session[:user_id]
-      redirect_to session.delete(:return_to) || projects_path
+      redirect_to session.delete(:return_to) || root_path
     else
       render :new
     end
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
     if user.try(:authenticate, params[:password])
       session[:user_id] = user.id
       flash.discard
-      redirect_to session.delete(:return_to) || projects_path
+      redirect_to session.delete(:return_to) || root_path
     else
       redirect_to login_url, alert: "Invalid email/password combination"
     end
