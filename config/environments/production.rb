77fb3e617d375f38795afe4d200f49d19eb4456d
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-09-01 13:05:53 -0700

    Use view caching for collections

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,11 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_cache_store, {
+    url: ENV['REDIS_URL'],
+    expires_in: 5.days,
+    size: 25.megabytes
+  }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
